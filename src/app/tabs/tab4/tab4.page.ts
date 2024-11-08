a100c6e37f45ab7787ecdfa5ef1b7ca3bf5ff673
AinizeGonUri <ainigonzalez@birt.eus>
2024-11-08 11:57:24 +0100

    DI-TE-6

--- src/app/tabs/tab4/tab4.page.ts
+++ src/app/tabs/tab4/tab4.page.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface IGameState {
+  numberToGuess: number;
+  userInput: number;
+  message: string;
+}
+
 @Component({
   selector: 'app-tab4',
   templateUrl: './tab4.page.html',
@@ -7,9 +13,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab4Page implements OnInit {
 
-  constructor() { }
+  gameState: IGameState;
+
+  constructor() {
+    // Inicializamos el estado del juego
+    this.gameState = {
+      numberToGuess: 0,
+      userInput: 0,
+      message: 'Introduce un número entre 0 y 100',
+    };
+  }
 
   ngOnInit() {
+    // Generamos un número aleatorio al cargar la página
+    this.gameState.numberToGuess = Math.floor(Math.random() * 101); // Número aleatorio entre 0 y 100
+  }
+
+  // Método que se ejecuta cuando el usuario hace clic en el botón
+  checkGuess() {
+    const { numberToGuess, userInput } = this.gameState;
+
+    if (userInput < 0 || userInput > 100) {
+      this.gameState.message = "Introduce un número entre 0 y 100";
+    } else if (userInput === numberToGuess) {
+      this.gameState.message = "Has acertado";
+    } else if (userInput < numberToGuess) {
+      this.gameState.message = "Introduce un número mayor";
+    } else if (userInput > numberToGuess) {
+      this.gameState.message = "Introduce un número menor";
+    }
   }
 
 }
