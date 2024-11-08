9836e27cc24626ef5c4af6cac296e1ba233ba69c
AinizeGonUri <ainigonzalez@birt.eus>
2024-11-08 11:30:55 +0100

    DI-TE-5

--- src/app/tabs/tab5/tab5.page.ts
+++ src/app/tabs/tab5/tab5.page.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface IValor {
+  imagen: string;
+  url: string;
+}
 @Component({
   selector: 'app-tab5',
   templateUrl: './tab5.page.html',
   styleUrls: ['./tab5.page.scss'],
 })
+
 export class Tab5Page implements OnInit {
 
   constructor() { }
@@ -12,4 +17,24 @@ export class Tab5Page implements OnInit {
   ngOnInit() {
   }
 
+  valoresLista: IValor[] = [
+    {
+      imagen: "BIRT_LOGO.png",
+      url: "https://birt.eus",
+    },
+    {
+      imagen: "easo_logo.png",
+      url: "https://easo.hezkuntza.net",
+    },
+    {
+      imagen: "nicolas_larburu_logo.jpg",
+      url: "https://nlarburu.hezkuntza.net",
+    },
+    {
+      imagen: "ciudad_jardin_logo.png",
+      url: "https://ciudadjardin.hezkuntza.net",
+    }
+  ];
+
 }
+
